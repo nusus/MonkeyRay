9f7bd367bff376179209b55167be52814d187217
HuangRui <471044801@qq.com>
2016-03-21 13:58:49 +0800

    io处理

---
+++ MoneyRay/MonkeyRay/CMRGUIEventAdapter.cpp
@@ -0,0 +1,25 @@
+#include "CMRGUIEventAdapter.h"
+
+MR::CMRGUIEventAdapter::CMRGUIEventAdapter()
+{
+}
+
+void MR::CMRGUIEventAdapter::setWindowRectangle(int x, int y, int width, int height, bool updateMouseRange)
+{
+}
+
+void MR::CMRGUIEventAdapter::setInputRange(float Xmin, float Ymin, float Xmax, float Ymax)
+{
+}
+
+MR::CMRGUIEventAdapter::CMRGUIEventAdapter(const CMRGUIEventAdapter & rhs, const osg::CopyOp & copyop)
+{
+}
+
+void MR::setMouseYOrientationAndUpdateCoords(MouseYOrientation myo)
+{
+}
+
+void MR::copyPointerDataFrom(const osgGA::GUIEventAdapter & sourceEvent)
+{
+}
